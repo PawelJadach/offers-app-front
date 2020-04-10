0a511c795604ebd0b64db6fadba8209b85bcc894
PawelJadach <pawel.jadach.96@gmail.com>
2020-04-10 12:48:39 +0200

    Updates

---
+++ precache-manifest.b563732b3887632280f67412f67ab349.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3e474ea504b2b97fde2eba7d7b6b6b",
+    "url": "/offers-app-front/index.html"
+  },
+  {
+    "revision": "1a7f4c3b65a576f1d874",
+    "url": "/offers-app-front/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "0ccfd9ec4a8e87f2a8b8",
+    "url": "/offers-app-front/static/css/main.d68335c3.chunk.css"
+  },
+  {
+    "revision": "1a7f4c3b65a576f1d874",
+    "url": "/offers-app-front/static/js/2.4324a099.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/offers-app-front/static/js/2.4324a099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ccfd9ec4a8e87f2a8b8",
+    "url": "/offers-app-front/static/js/main.764017ce.chunk.js"
+  },
+  {
+    "revision": "b346144d7c932484ed96",
+    "url": "/offers-app-front/static/js/runtime-main.8b7943b8.js"
+  }
+]);
