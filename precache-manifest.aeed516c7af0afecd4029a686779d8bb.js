f9fc9a6aba5d2d1cfd54bf8c0420b9f4901d7e7e
PawelJadach <pawel.jadach.96@gmail.com>
2020-04-23 06:29:51 +0200

    Updates

---
+++ precache-manifest.aeed516c7af0afecd4029a686779d8bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9906859b9d13f3a7b72e9d8c330fef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0268ecf7bac57b8bdd16",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "b7b5e502b2a254d0c154",
+    "url": "/static/css/main.4c5b3027.chunk.css"
+  },
+  {
+    "revision": "0268ecf7bac57b8bdd16",
+    "url": "/static/js/2.789cdeda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.789cdeda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b5e502b2a254d0c154",
+    "url": "/static/js/main.a70c011e.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
